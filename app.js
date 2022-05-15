5c66fa2bbb1b70332728ae2e98992877d1e87c7d
JenLynX <104281812+JenLynX@users.noreply.github.com>
2022-05-15 15:15:31 -0700

    created forecast function

--- app.js
+++ app.js
@@ -44,6 +44,19 @@ if (minutes < 10) {
 h2.innerHTML = `${day}, ${month} ${date},  ${year}`;
 h3.innerHTML = `${hours}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function getForecast(coordinates) {
+  let apiKey = "ed1d03d83986af8d64630bead0e0f624";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemp(response) {
   celsiusTemperature = response.data.main.temp;
   document.querySelector("#city-input").innerHTML = response.data.name;
@@ -65,6 +78,39 @@ function showTemp(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastDays = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+    <div class="col-2" >
+    <div class="container" id="forecast">
+         
+            <div id="day-style">
+              <div class="day">${formatDay(forecastDay.dt)}</div>
+              <div class ="daysTemp"><strong>${Math.round(
+                forecastDay.temp.max
+              )}°</strong></div>
+              <div class="icon"><img src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png" id="days-emoji" /></div>
+              <div class="night">Low ${Math.round(forecastDay.temp.min)}°</div>
+            </div></div></div>
+            `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastDays.innerHTML = forecastHTML;
 }
 
 function search(city) {
